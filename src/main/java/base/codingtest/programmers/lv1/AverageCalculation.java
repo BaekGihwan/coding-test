7792639b6c0f1d9a0bb233514bd8acb3e91c34d8
neur0n <ghbaek88@gmail.com>
2026-01-04 13:48:01 +0900

    프로그래머스 Lv1 평균 구하기

---
+++ src/main/java/base/codingtest/programmers/lv1/AverageCalculation.java
@@ -0,0 +1,28 @@
+package base.codingtest.programmers.lv1;
+
+public class AverageCalculation {
+    // 평균 구하기
+    // https://school.programmers.co.kr/learn/courses/30/lessons/12944
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {1,2,3,4};
+
+        double result = solution(arr);
+        System.out.println("answer is " + result);
+    }
+
+    /**
+     * 짝수와 홀수
+     * @param arr 입력값
+     * @return 평균 값
+     */
+    private static double solution(int[] arr) {
+        double sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+        }
+
+        return sum / arr.length;
+    }
+}
