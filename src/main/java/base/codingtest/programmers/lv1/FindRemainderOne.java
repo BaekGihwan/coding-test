148bfe5cb3695df1231d9fa5ee3877fc4fe6972c
neur0n <ghbaek88@gmail.com>
2026-01-05 17:16:32 +0900

    프로그래머스 Lv1 나머지가 1이 되는 수 찾기

---
+++ src/main/java/base/codingtest/programmers/lv1/FindRemainderOne.java
@@ -0,0 +1,30 @@
+package base.codingtest.programmers.lv1;
+
+import java.util.Scanner;
+
+public class FindRemainderOne {
+    // 나머지가 1이 되는 수 찾기
+    // https://school.programmers.co.kr/learn/courses/30/lessons/87389
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("자연수 n를 입력하세요: ");
+        int n = scanner.nextInt();
+
+        int result = solution(n);
+
+        System.out.println("answer is " + result);
+
+        scanner.close();
+    }
+
+    private static int solution(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
